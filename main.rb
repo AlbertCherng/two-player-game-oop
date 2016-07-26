b226653fab0d2557d2b08d15cb30852f3e807a49
vagrant <albert.cherng@gmail.com>
2016-07-26 18:51:42 +0000

    Add game, player, question file

---
+++ main.rb
@@ -0,0 +1,10 @@
+require ("./question")
+require ("./question")
+require ("./question")
+require ("./question")
+require ("./question")
+
+
+round1 = Question.new
+puts round1.question_maker
+puts round1.question_answer
---
+++ question.rb
@@ -0,0 +1,25 @@
+class Question
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @sum = @num1 + @num2
+
+  end
+
+  def get_num1
+    @num1
+  end
+
+  def get_num2
+    @num2
+  end
+
+  def question_maker
+    "What is #{@num1} + #{@num2}?"
+  end
+
+  def question_answer
+    @sum
+  end
+end
+
